5260c76738867fb65a59562b0e7536c2ea04b1b7
Waseem Akram <khalilah64780@gmail.com>
2026-01-18 06:01:47 +0000

    Ab Main Apne app ke liye ek Android web view app banaunga jismein main s

---
+++ android/app/src/main/java/com/ludoleague/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ludoleague;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // Load your Next.js app URL here
+        myWebView.loadUrl("https://studio--studio-4431476254-c1156.us-central1.hosted.app");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
