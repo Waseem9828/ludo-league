c93c8648567ebaebc4bc312a4ecc3da103df769a
Waseem Akram <khalilah64780@gmail.com>
2026-01-22 04:32:09 +0000

    Dashboard card images upload nahi ho rahi hai

--- src/lib/image-utils.ts
+++ src/lib/image-utils.ts
@@ -1,10 +1,3 @@
-import imageCompression from 'browser-image-compression';
-
-const defaultOptions = {
-    maxSizeMB: 1,
-    maxWidthOrHeight: 1920,
-    useWebWorker: false,
-};
 
 /**
  * Compresses an image file.
@@ -12,13 +5,6 @@ const defaultOptions = {
  * @returns {Promise<File>} The compressed file.
  */
 export const compressImage = async (file: File): Promise<File> => {
-    try {
-        const compressedFile = await imageCompression(file, defaultOptions);
-        console.log(`Compressed ${file.name} from ${file.size / 1024 / 1024} MB to ${compressedFile.size / 1024 / 1024} MB`);
-        return compressedFile;
-    } catch (error) {
-        // console.error(`Could not compress image ${file.name}, returning original file.`, error);
-        // If compression fails, return the original file
-        return file;
-    }
+    // Bypassing compression due to environment issues. Returning original file.
+    return file;
 };
